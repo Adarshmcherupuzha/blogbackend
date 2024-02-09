0ecb29e4ef4918ae5a18b0e4cf3c75f4318f5059
Adarshmcherupuzha <adarshmpadichal@gmail.com>
2024-02-09 14:49:10 +0530

    post completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require("express")
 const cors=require("cors")
 const mongoose=require("mongoose")
 const userrouter=require("./controller/usersrouter")
+const postrouter=require("./controller/postrouter")
 
 const app=express()
 app.use(express.json())
@@ -11,6 +12,8 @@ app.use(cors())
 mongoose.connect("mongodb+srv://adarsh:adarsh123@cluster0.bsql3fl.mongodb.net/userblogDb?retryWrites=true&w=majority",
 {useNewurlParser:true})
 app.use("/api/blog",userrouter)
+
+app.use("/api/post",postrouter)
 
 app.listen(3000,()=>{
     console.log("server running")
